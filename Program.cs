07109eaeca2e7794832524551bdfc315c7242e1b
moored <moored@V10RM126W10-02>
2022-09-26 10:34:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1To10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+            Console.WriteLine(a++);
+        }
+    }
+}
